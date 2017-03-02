b414eba8c0a21c8b3d2663f44fb2a95e6e5435d4
George Danforth <georgejdanforth@gmail.com>
2017-03-01 19:44:25 -0500

    Refactor database operations

--- fob/db.py
+++ fob/db.py
@@ -1,4 +1,32 @@
 import sqlite3
 
 
-# TODO: Refactor all db handling code into here
+DB_PATH = "fob.db"  # TODO: Change this to a better location later
+
+
+def insert_query(table, columns, values):
+    query = " ".join([
+        "INSERT INTO",
+        table,
+        columns,
+        "VALUES",
+        "({})".format(", ".join(["?"] * len(values)))
+    ])
+
+    # TODO: Add exception handling
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.cursor()
+        cursor.execute(query, values)
+
+
+def select_query(value, column="service_name"):
+    query = " ".join(["SELECT * FROM passwords WHERE", column, "= ?"])
+
+    # TODO: Add exception handling
+    with sqlite3.connect(DB_PATH) as conn:
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute(query, value)
+        result = cursor.fetchone()
+
+    return result
--- fob/fob.py
+++ fob/fob.py
@@ -1,7 +1,8 @@
+import db
+
 import os
 import base64
 import hashlib
-import sqlite3
 
 from cryptography.fernet import Fernet, InvalidToken
 from cryptography.exceptions import InvalidSignature
@@ -18,17 +19,8 @@ def config_db():
     fob_password = bytes_input("Enter a password to use for fob: ")
     hashed_fob_password = hash_fob_password(fob_password)
 
-    with sqlite3.connect("fob.db") as conn:
-        cursor = conn.cursor()
-        cursor.execute(
-            "INSERT INTO fob_passwords (fob_password) VALUES (?)",
-            (hashed_fob_password,)
-        )
-        cursor.execute(
-            "INSERT INTO salts (salt) VALUES (?)",
-            (os.urandom(16),)
-        )
-        conn.commit()
+    db.insert_query("fob_passwords", "(fob_password)", (hashed_fob_password,))
+    db.insert_query("salts", "(salt)", (os.urandom(16),))
 
 
 def hash_fob_password(fob_password):
@@ -81,34 +73,27 @@ def add_password():
     hashed_account_name = fernet.encrypt(account_name)
     hashed_password = fernet.encrypt(password)
 
-    with sqlite3.connect("fob.db") as conn:
-        cursor = conn.cursor()
-        cursor.execute("""
-            INSERT INTO passwords
-            (service_name, service_url, account_name, password)
-            VALUES (?, ?, ?, ?)""",
-            (service_name, service_url, hashed_account_name, hashed_password)
-        )
-        conn.commit()
+    db.insert_query(
+        "passwords",
+        "(service_name, service_url, account_name, password)",
+        (service_name, service_url, hashed_account_name, hashed_password)
+    )
 
 
-def retreive_password(service_name):
+def retreive_password():
     fob_password = bytes_input("Enter fob password: ")
+    service_name = input("Service name: ")
     if hash_fob_password(fob_password) != get_hashed_fob_password():
         # TODO: handle error properly
         quit()
 
     fernet = create_fernet_obj(fob_password)
 
-    with sqlite3.connect("fob.db") as conn:
-        conn.row_factory = sqlite3.Row
-        cursor = conn.cursor()
-        cursor.execute("""
-            SELECT * FROM passwords
-            WHERE service_name = ?""",
-            (service_name,)
-        )
-        result = cursor.fetchone()
+    result = db.select_query((service_name,))
+    if result is None:
+        # TODO: handle error properly
+        quit()
+
     result = {key: result[key] for key in result.keys()}
     result["account_name"] = fernet.decrypt(result["account_name"])
     result["password"] = fernet.decrypt(result["password"])
@@ -116,9 +101,8 @@ def retreive_password(service_name):
 
 
 def main():
-    #config_db()
-    #add_password()
-    print(retreive_password(input("service name: ").strip()))
+    # TODO: Logic w/ argparse
+    pass
 
 
 if __name__ == "__main__":
