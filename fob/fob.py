f46015c6411e21bc151246fe33b80453e8fc2c6d
George Danforth <georgejdanforth@gmail.com>
2017-03-10 00:21:28 -0500

    Command line argument and password display tempfile handling
    this project is starting to resemble something that actually works!

--- fob/fob.py
+++ fob/fob.py
@@ -1,4 +1,7 @@
 import os
+import sys
+import getpass
+import tempfile
 
 import db
 import crypto
@@ -8,16 +11,40 @@ def bytes_input(prompt):
     return input(prompt).encode("utf-8")
 
 
-def config_db():
-    fob_password = bytes_input("Enter a password to use for fob: ")
-    hashed_fob_password = crypto.hash_fob_password(fob_password)
+def get_fob_password(prompt="Fob Password: "):
+    return getpass.getpass(prompt=prompt).encode("utf-8")
 
-    db.insert("fob_passwords", "(fob_password)", (hashed_fob_password,))
-    db.insert("salts", "(salt)", (os.urandom(16),))
+
+def config_db():
+    fob_password_1 = get_fob_password("Enter a password to use with fob: ")
+    fob_password_2 = get_fob_password("Verify your password: ")
+
+    if fob_password_1 != fob_password_2:
+        print("Passwords do not match! Try again.")
+    else:
+        hashed_fob_password = crypto.hash_fob_password(fob_password_1)
+
+        db.insert("fob_passwords", "(fob_password)", (hashed_fob_password,))
+        db.insert("salts", "(salt)", (os.urandom(16),))
+
+
+def help_message():
+    print("usage: fob <command> <argument>")
+
+
+def write_read_password_tempfile(result):
+    # TODO: exception handling
+    fd, fname = tempfile.mkstemp(text=True)
+    f = os.fdopen(fd, "w")
+    for key in result.keys():
+        f.write(" ".join([key, "-", result[key], "\n"]))
+    f.close()
+    os.system("$EDITOR " + fname)    
+    os.unlink(fname)
 
 
 def add_password():
-    fob_password = bytes_input("Enter fob password: ")
+    fob_password = get_fob_password()
     if crypto.hash_fob_password(fob_password) != db.select_single("fob_passwords"):
         # TODO: handle error properly
         quit()
@@ -38,7 +65,7 @@ def add_password():
 
 
 def retreive_password():
-    fob_password = bytes_input("Enter fob password: ")
+    fob_password = get_fob_password()
     service_name = input("Service name: ")
     if crypto.hash_fob_password(fob_password) != db.select_single("fob_passwords"):
         # TODO: handle error properly
@@ -49,15 +76,28 @@ def retreive_password():
         # TODO: handle error properly
         quit()
 
-    result = {key: result[key] for key in result.keys()}
-    result["account_name"] = crypto.decrypt(fob_password, result["account_name"])
-    result["password"] = crypto.decrypt(fob_password, result["password"])
-    return result
+    result = {key: result[key] for key in result.keys() if key != "password_id"}
+
+    # lol @ these 2 lines
+    for field in ["account_name", "password"]:
+        result[field] = str(crypto.decrypt(fob_password, result[field]))
+
+    write_read_password_tempfile(result)
+
+
+commands = {
+    "add": add_password,
+    "get": retreive_password,
+    "help": help_message,
+}
 
 
 def main():
-    # TODO: Logic w/ argparse
-    print(retreive_password())
+    # TODO: check if configured before execution
+    if len(sys.argv) <= 1 or sys.argv[1] not in commands.keys():
+        help_message()
+    else:
+        commands[sys.argv[1]]()
 
 
 if __name__ == "__main__":
